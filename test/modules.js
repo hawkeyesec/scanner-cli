3def74fa2f8cbe35506d0e9e4ec75d6533baccf2
Karl Stoney <me@karlstoney.com>
2017-03-19 13:32:33 +0000

    refactor

--- test/modules.js
+++ test/modules.js
@@ -1,18 +1,12 @@
 'use strict';
-const glob = require('glob');
-const path = require('path');
 require('should');
+const deride = require('deride');
 
 describe('Modules', () => {
-  let pathToModules = path.join(__dirname, '../lib/modules/*/**/index.js');
-  let files = glob.sync(pathToModules);
-
-  files.forEach(file => {
-    describe(file, () => {
-      let module;
-      before(() => {
-        module = new require(file)();
-      });
+  new require('../lib/modules')({
+    logger: deride.stub(['log'])
+  }).asArray.forEach(module => {
+    describe(module.name, () => {
       it('should have the module signature', () => {
         let expectMethods = ['key', 'name', 'description', 'handles', 'run', 'enabled'].sort();
         Object.keys(module).sort().should.eql(expectMethods);
