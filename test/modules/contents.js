39a8468c92ef2915a8800971c21e0725c0dcfd84
Karl Stoney <me@karlstoney.com>
2017-10-24 11:21:22 +0000

    Addresses issue #16 This was a nested async callstack issue, see:
      - See: https://github.com/caolan/async/issues/1265

--- test/modules/contents.js
+++ test/modules/contents.js
@@ -6,18 +6,19 @@ const should = require('should');
 const FileManager = require('../../lib/fileManager');
 
 describe('Contents', () => {
-  let contents, mockResults;
+  let contents, mockResults, fileManager;
   beforeEach(() => {
     mockResults = deride.stub(['low', 'medium', 'high', 'critical']);
     const nullLogger = deride.stub(['log', 'debug', 'error']);
-    const fileManager = new FileManager({
-      target: path.join(__dirname, '../samples/nodejs'),
-      logger: nullLogger
-    });
 
     contents = new Contents({
       patterns: path.join(__dirname, '../samples/contents.js')
     });
+    fileManager = deride.wrap(new FileManager({
+      target: path.join(__dirname, '../samples/nodejs'),
+      logger: nullLogger
+    }));
+
     should(contents.handles(fileManager)).eql(true);
   });
 
@@ -25,6 +26,13 @@ describe('Contents', () => {
     mockResults.setup.critical.toDoThis(data => {
       should(data.offender).eql('some_file_with_private_key_in.md');
     });
+    contents.run(mockResults, done);
+  });
+
+  it('should not explode when there are more than 115 files', done => {
+    for(var x = 0; x < 3000; x++) {
+      fileManager.languageFiles.push(fileManager.languageFiles[1]);
+    }
     contents.run(mockResults, done);
   });
 });
