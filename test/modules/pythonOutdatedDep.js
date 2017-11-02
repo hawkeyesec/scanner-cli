4ca5f0ac979c3fcb2e955ce807907e0b14900638
nicolastrres <nicolastrres@gmail.com>
2017-11-01 23:54:29 -0300

    Added python-outdated-dep module to scan using piprot the python dependencies

---
+++ test/modules/pythonOutdatedDep.js
@@ -0,0 +1,101 @@
+'use strict';
+const PythonOutdatedDep = require('../../lib/modules/python-outdated-dep');
+const FileManager = require('../../lib/fileManager');
+const deride = require('deride');
+const path = require('path');
+const should = require('should');
+const fs = require('fs');
+
+
+describe('PythonOutdatedDep', () => {
+  const sample = fs.readFileSync(path.join(__dirname, '../samples/piprot.txt'), 'utf8');
+
+  let pythonOutdatedDep, mockExec, mockResults, fileManager;
+  beforeEach(() => {
+    mockExec = deride.stub(['command', 'commandExists']);
+    mockExec.setup.command.toCallbackWith(null, {
+      stdout: sample
+    });
+    mockExec.setup.commandExists.toReturn(true);
+
+    const nullLogger = deride.stub(['log', 'debug', 'error']);
+    fileManager = new FileManager({
+      target: path.join(__dirname, '../samples/python'),
+      logger: nullLogger
+    });
+    mockResults = deride.stub(['low', 'medium', 'high', 'critical']);
+    pythonOutdatedDep = new PythonOutdatedDep({
+      exec: mockExec
+    });
+    should(pythonOutdatedDep.handles(fileManager)).eql(true);
+  });
+
+  it('should execute piprot -o', done => {
+    pythonOutdatedDep.run(mockResults, () => {
+      mockExec.expect.command.called.withArg('piprot -o');
+      done();
+    });
+  });
+
+  it('should log major version changes as high', done => {
+    mockResults.setup.high.toDoThis(data => {
+      should(data.offender).eql('cryptography');
+      should(data.code).eql(1);
+    });
+    pythonOutdatedDep.run(mockResults, done);
+  });
+
+  it('should log minor version changes as medium', done => {
+    mockResults.setup.medium.toDoThis(data => {
+      should(data.offender).eql('pytest');
+      should(data.code).eql(2);
+    });
+    pythonOutdatedDep.run(mockResults, done);
+  });
+
+  it('should log patch version changes as low', done => {
+    pythonOutdatedDep.run(mockResults, () => {
+      const item = {
+        code: 3,
+        offender: 'email_validator',
+        description: 'Module is one or more patch versions out of date',
+        mitigation: 'Update to 1.0.3'
+      }
+
+      mockResults.expect.low.called.withArgs(item);
+      done();
+    });
+  });
+
+  it('should log not valid versions as low', done => {
+    pythonOutdatedDep.run(mockResults, () => {
+      const item = {
+        code: 3,
+        offender: 'psycopg2',
+        description: 'Module is one or more patch versions out of date',
+        mitigation: 'Update to 2.7.3.2'
+      }
+
+      mockResults.expect.low.called.withArgs(item);
+      done();
+    });
+  });
+
+  it('should not run piprot if not installed', done => {
+    const mockExec = deride.stub(['commandExists']);
+    const mockLogger = deride.stub(['warn']);
+    mockExec.setup.commandExists.toReturn(false);
+
+    const pythonOutdatedDep = new PythonOutdatedDep({
+      exec: mockExec,
+      logger: mockLogger
+    });
+
+    should(pythonOutdatedDep.handles(fileManager)).eql(false);
+    mockLogger.expect.warn.called.withArgs('requirements.txt found but piprot not found in $PATH');
+    mockLogger.expect.warn.called.withArgs('pythonOutdatedDepScan will not run unless you install piprot');
+    mockLogger.expect.warn.called.withArgs('Please see: https://github.com/sesh/piprot');
+    done();
+  });
+
+});
