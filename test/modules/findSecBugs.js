b1c39c8cf9285f1f194b4174b447dd3097c04dea
nicolastrres <nicolastrres@gmail.com>
2017-11-30 18:35:41 -0300

    Creates findSecBugs module to scan for potential security issues on java projects

---
+++ test/modules/findSecBugs.js
@@ -0,0 +1,103 @@
+'use strict';
+const FindSecBugs = require('../../lib/modules/findSecBugs');
+const FileManager = require('../../lib/fileManager');
+const deride = require('deride');
+const path = require('path');
+const should = require('should');
+const fs = require('fs');
+
+
+describe('FindSecBugs', () => {
+  let findSecBugs, mockExec, mockResults, fileManager;
+  beforeEach(() => {
+    mockExec = deride.stub(['command', 'commandExists']);
+    mockExec.setup.command.toCallbackWith(null, {
+      stdout: null
+    });
+    mockExec.setup.commandExists.toReturn(true);
+
+    const nullLogger = deride.stub(['log', 'debug', 'error']);
+    fileManager = new FileManager({
+      target: path.join(__dirname, '../samples/java'),
+      logger: nullLogger
+    });
+
+    fileManager = deride.wrap(fileManager);
+    const sampleReport = fs.readFileSync(path.join(__dirname, '../samples/findSecBugsReport.xml'), 'utf-8');
+    fileManager.setup.readFileSync.toReturn(sampleReport);
+
+    mockResults = deride.stub(['low', 'medium', 'high', 'critical']);
+    findSecBugs = new FindSecBugs({
+      exec: mockExec
+    });
+
+    should(findSecBugs.handles(fileManager)).eql(true);
+  });
+
+  it('should execute findsecbugs -nested:false -progress -effort:max -exitcode -xml:withMessages -output /target/findSecBugsReport.xml -low /target', done => {
+    findSecBugs.run(mockResults, () => {
+      mockExec.expect.command.called.withArg('findsecbugs -nested:false -progress -effort:max -exitcode -xml:withMessages -output /target/findSecBugsReport.xml -low /target');
+      done();
+    });
+  });
+
+  it('should log issues with HIGH severity as high', done => {
+    findSecBugs.run(mockResults, () => {
+      const item = {
+        code: 'XML_DECODER',
+        offender: 'In method com.hawkeye.java.test.controller.MyVulnerableControllerClass.Update(int, UpdateCommand, BindingResult)',
+        description: 'XMLDecoder usage',
+        mitigation: 'Check lines [48]'
+      };
+
+      mockResults.expect.high.called.withArgs(item);
+      done();
+    });
+  });
+
+  it('should log issues with MEDIUM severity as medium', done => {
+    findSecBugs.run(mockResults, () => {
+      const item = {
+        code: 'PREDICTABLE_RANDOM',
+        offender: 'In method com.hawkeye.java.test.config.MyVulnerableConfigClass.generateSecretToken()',
+        description: 'Predictable pseudorandom number generator',
+        mitigation: 'Check lines [30]'
+      };
+
+      mockResults.expect.medium.called.withArgs(item);
+      done();
+    });
+  });
+
+  it('should log issues with LOW severity as low', done => {
+    findSecBugs.run(mockResults, () => {
+      const item = {
+        code: 'COOKIE_USAGE',
+        offender: 'In method com.hawkeye.java.test.controller.MyVulnerableControllerClass.Update(int, UpdateCommand, BindingResult)',
+        description: 'Potentially sensitive data in a cookie',
+        mitigation: 'Check lines [44]'
+      };
+
+      mockResults.expect.low.called.withArgs(item);
+      done();
+    });
+  });
+
+  it('should not run findSecBugs if not installed', done => {
+    const mockExec = deride.stub(['commandExists']);
+    const mockLogger = deride.stub(['warn']);
+    mockExec.setup.commandExists.toReturn(false);
+
+    const findSecBugs = new FindSecBugs({
+      exec: mockExec,
+      logger: mockLogger
+    });
+
+    should(findSecBugs.handles(fileManager)).eql(false);
+    mockLogger.expect.warn.called.withArgs('pom.xml found but findSecBugs was not found in $PATH');
+    mockLogger.expect.warn.called.withArgs('findSecBugs scan will not run unless you install findSecBugs CLI');
+    mockLogger.expect.warn.called.withArgs('Please see: https://github.com/find-sec-bugs/find-sec-bugs/wiki/CLI-Tutorial');
+    done();
+  });
+
+});
