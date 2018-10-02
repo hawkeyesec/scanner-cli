b74173ed6d5ed3fa1f5edc7c62ba23f262dcf470
Felix Hammerl <fhammerl@thoughtworks.com>
2018-10-02 15:11:13 +0200

    Turns out npm version will also create a version commit and tag

--- scripts/bump-version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")/.." || exit 1
-
-# read version from package.json and trim leading/trailing whitespace
-VERSION="v$(cat package.json | awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/version\042/){print $(i+1)}}}' | tr -d '"' | sed -n 1p | sed -e 's/^ *//' -e 's/ *$//')"
-echo "Tagging: $VERSION"
-git tag "$VERSION"
