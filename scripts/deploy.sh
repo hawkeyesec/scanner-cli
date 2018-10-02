6903ce2555644db4bd1f5df4810733acd4bde35f
Felix Hammerl <fhammerl@thoughtworks.com>
2018-10-02 15:00:30 +0200

    Simplify version management via npm version hooks

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-echo "Branch is: master.  Building and releasing container."
+cd "$(dirname "$0")/.." || exit 1
 
-json_value() {
-  KEY=$1
-  num=$2
-  awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p
-}
+VERSION=$(cat package.json | awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/version\042/){print $(i+1)}}}' | tr -d '"' | sed -n 1p | sed -e 's/^ *//' -e 's/ *$//')
+echo "Building and releasing container version $VERSION ..."
 
-HE_VERSION=${cat package.json | json_value version 1 | sed -e 's/^ *//' -e 's/ *$//'}
 docker pull stono/hawkeye
 docker build -t stono/hawkeye . --cache-from stono/hawkeye
-docker tag stono/hawkeye:latest stono/hawkeye:"$HE_VERSION"
+docker tag stono/hawkeye:latest stono/hawkeye:"$VERSION"
 docker login  -p="$DOCKER_PASSWORD"
 echo "$DOCKER_PASSWORD" | docker login -u -u="$DOCKER_USERNAME" --password-stdin
 docker push stono/hawkeye:latest
-docker push stono/hawkeye:"$HE_VERSION"
+docker push stono/hawkeye:"$VERSION"
