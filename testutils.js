8b698dea2615b858bce6f67ea962023f7ae17efa
Felix Hammerl <fhammerl@thoughtworks.com>
2018-10-03 18:18:15 +0200

    Use sinon-chai

--- testutils.js
+++ testutils.js
@@ -1,6 +1,13 @@
 const chai = require('chai')
 const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+
+chai.use(sinonChai)
 
 global.expect = chai.expect
 global.sinon = sinon
 global.request = chai.request
+
+after(() => {
+  sinon.verify()
+})
