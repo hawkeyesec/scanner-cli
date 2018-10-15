90512faf5d0ed4687e23c191d32fd4227d6ba08e
Felix Hammerl <fhammerl@thoughtworks.com>
2018-10-15 16:19:26 +0200

    Rework API of java-find-secbugs module

--- testutils.js
+++ testutils.js
@@ -1,13 +1,15 @@
 const chai = require('chai')
 const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
+const chaiAsPromised = require('chai-as-promised')
 
 chai.use(sinonChai)
+chai.use(chaiAsPromised)
 
 global.expect = chai.expect
 global.sinon = sinon
 global.request = chai.request
 
-after(() => {
-  sinon.verify()
+afterEach(() => {
+  sinon.verifyAndRestore()
 })
