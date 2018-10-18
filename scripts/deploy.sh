3ac84eff8b75ae1f58f25becb12ae70dd8527c61
Felix Hammerl <fhammerl@thoughtworks.com>
2018-10-18 17:51:46 +0200

    Fix deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,9 +5,8 @@ cd "$(dirname "$0")/.." || exit 1
 VERSION=$(cat package.json | awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/version\042/){print $(i+1)}}}' | tr -d '"' | sed -n 1p | sed -e 's/^ *//' -e 's/ *$//')
 echo "Building and releasing container version $VERSION ..."
 
-docker pull stono/hawkeye
-docker build -t stono/hawkeye . --cache-from stono/hawkeye
-docker tag stono/hawkeye:latest stono/hawkeye:"$VERSION"
+docker build -t hawkeyesec/scanner-cli .
+docker tag hawkeyesec/scanner-cli:latest hawkeyesec/scanner-cli:"$VERSION"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push stono/hawkeye:latest
-docker push stono/hawkeye:"$VERSION"
+docker push hawkeyesec/scanner-cli:latest
+docker push hawkeyesec/scanner-cli:"$VERSION"
