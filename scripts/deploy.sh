8273ed1a6a53abf3ca4ad710d041ebbee65beb5b
Felix Hammerl <felix.hammerl@gmail.com>
2018-10-19 11:03:48 +0200

    Fix deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,7 @@ cd "$(dirname "$0")/.." || exit 1
 VERSION=$(cat package.json | awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/version\042/){print $(i+1)}}}' | tr -d '"' | sed -n 1p | sed -e 's/^ *//' -e 's/ *$//')
 echo "Building and releasing container version $VERSION ..."
 
-docker build -t hawkeyesec/scanner-cli .
-docker tag hawkeyesec/scanner-cli:latest hawkeyesec/scanner-cli:"$VERSION"
+docker build . -t hawkeyesec/scanner-cli:latest -t hawkeyesec/scanner-cli:"$VERSION"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push hawkeyesec/scanner-cli:latest
 docker push hawkeyesec/scanner-cli:"$VERSION"
+docker push hawkeyesec/scanner-cli:latest
